e8f12799a061575a4dfab7d2c22b732ea1eb199f
Cheng Lou <chenglou92@gmail.com>
2025-12-01 01:24:07 -0800

    Show an easy puzzle

--- show.py
+++ show.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from debug import print_sudoku
 
-sample = pd.read_csv("data/sudoku-3m.csv", nrows=1).iloc[0]
+df = pd.read_csv("data/sudoku-3m.csv", nrows=10000)
+sample = df[df['difficulty'] == df['difficulty'].min()].iloc[1] # relatively easy one
 
 print(f"id: {sample['id']}, clues: {sample['clues']}, difficulty: {sample['difficulty']}")
 print("\nPuzzle:")
